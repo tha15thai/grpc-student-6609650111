083c36c8a95d36b87c6d0888b4be6342ef3a8dfd
Thawanhathai Thiamthong - 6609650111 <thawanhathai.thi@dome.tu.ac.th>
2026-03-30 15:58:41 +0700

    feat: update client to support ListStudents and display phone field

--- client/client.go
+++ client/client.go
@@ -30,9 +30,21 @@ func main() {
 		log.Fatalf("Error calling GetStudent: %v", err)
 	}
 
+	resList, err := client.ListStudents(ctx, &pb.Empty{})
+	if err != nil {
+		log.Fatalf("Error calling ListStudents: %v", err)
+	}
+
+	log.Println("Student List:")
+	for _, s := range resList.Student {
+		log.Printf("ID: %d | Name: %s | Major: %s | Email: %s | Phone: %s",
+			s.Id, s.Name, s.Major, s.Email, s.Phone)
+	}
+
 	log.Printf("Student Info:")
 	log.Printf("ID: %d", res.Id)
 	log.Printf("Name: %s", res.Name)
 	log.Printf("Major: %s", res.Major)
 	log.Printf("Email: %s", res.Email)
+	log.Printf("Phone: %s", res.Phone)
 }
